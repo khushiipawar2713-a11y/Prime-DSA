0e18d401cc5742a1a53c4850c189abd675abda5d
khushiipawar2713-ally <khushiipawar2713@gmail.com>
2026-02-04 16:19:38 +0530

    Binary to Decimal

---
+++ PATTERNS.cpp/practiceQS.cpp
@@ -0,0 +1,109 @@
+
+//SQUARE PATTERNS
+// # include<iostream>
+//  using namespace std;
+ 
+// int main(){
+//     int n=5;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n;j++){
+//             cout<<j<<" ";
+//     }
+//     cout<<endl;
+// }
+//    return 0;
+//  }
+
+//  # include<iostream>
+//  using namespace std;
+ 
+// int main(){
+//     int n=5;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n;j++){
+//             cout<<"* ";
+//     }
+//     cout<<endl;
+// }
+//    return 0;
+//  }
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int n=3;
+//     for(int i=1;i<n;i++){
+//         char ch='A' ;
+//         for (int j=0;j<n;j++){
+//            cout<<ch;
+//              ch=ch+1;
+//     }
+//     cout<<endl;}
+//     return 0;
+// }
+
+//continuous patterns
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int n=4;
+
+//      char ch='A';
+
+//     for(int i=1;i<n;i++){
+//         for (int j=0;j<n;j++){
+//            cout<<ch<<" ";
+//              ch++;
+//     }
+//     cout<<endl;}
+//     return 0;
+//}
+
+ //#include <iostream>
+ //using namespace std;
+
+//int main(){
+  //  int n=5;
+    //int num=1;
+
+    //for(int i=0;i<n;i++){
+      //   for (int j=0;j<n;j++){
+        //   cout<<num<<"\t";
+          //    num++;
+     //}
+     //cout<<endl;}
+    //return 0;
+ //}
+
+//  #include<iostream>
+//  using namespace std;
+
+//  int main(){
+//    int n=4;
+//    for (int i=0;i<n;i++){
+//       for(int j=0;j<i+1;j++){
+//          cout<<"*";
+//          }
+// cout<<endl;
+//       }
+//       return 0;
+//  }
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//   int n=7;
+
+//   char ch='A';
+//   for(int i=0;i<n;i++){
+//     for(int j=i+1;j>0;j--){
+//       cout<<ch<<" ";
+//       ch++;    
+//   }
+//   cout<<endl;
+// }
+// return 0;
+// }
