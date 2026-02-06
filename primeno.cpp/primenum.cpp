c24e0352872775018e828ca97d9942a4af08bb7e
khushiipawar2713-ally <khushiipawar2713@gmail.com>
2026-02-06 16:28:26 +0530

    Notes

---
+++ primeno.cpp/primenum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+string isPrime(int n){
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0){
+            return"non prime";
+        }
+    }
+    return"prime";
+}
+int main(){
+int n=47;
+    cout<<isPrime(n)<<endl;
+    return 0;
+}
+
