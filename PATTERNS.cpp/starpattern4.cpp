b0be7190cc8ad1c6a09b3dd43cf7cac880e882ca
khushiipawar2713-ally <khushiipawar2713@gmail.com>
2026-02-08 16:22:53 +0530

    starpatternQ4

--- PATTERNS.cpp/starpattern4.cpp
+++ PATTERNS.cpp/starpattern4.cpp
@@ -2,13 +2,10 @@
 using namespace std;
 int main(){
     int n=5;
-        for(int i=1;i<=n;i++){
+        for(int i=n;i>=1;i--){
             for (int j=1;j<=i;j++){
-            cout<<"  ";
-}
-   for (int j=1;j<=n-i;j++){
-    cout<<"* ";
-   }
+            cout<<"*  "; 
+            }
 cout<<endl;
     }
     return 0;
