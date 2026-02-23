2b3227c1d5d9b0163c6b2188112a53774887c0f2
khushiipawar2713-ally <khushiipawar2713@gmail.com>
2026-02-23 19:52:59 +0530

    Assignment Shopping

---
+++ PATTERNS.cpp/ARRAY/shoppingassignment.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+		int m,n;
+		cin>>m>>n;
+		if(m<n){
+			cout<<"Harshit"<<endl;
+		}
+		else{
+			cout<<"Aayush"<<endl;
+		}
+	}
+	return 0;
+}
