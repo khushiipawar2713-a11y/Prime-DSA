cdd427e458a7e7d40f05559f72564d82091c355b
khushiipawar2713-ally <khushiipawar2713@gmail.com>
2026-03-13 16:55:06 +0530

    Character_array_basic

---
+++ PATTERNS.cpp/ARRAY/Vector/Matrix_Multiplication.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>>a(n,vector<int>(m));
+     vector<vector<int>>b(n,vector<int>(m));
+for(inti=0;i<n;i++)
+}
