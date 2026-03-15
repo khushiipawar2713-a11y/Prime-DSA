53c51b7136bd4144ee3d3ef3b165e75063e719ac
khushiipawar2713-ally <khushiipawar2713@gmail.com>
2026-03-15 14:34:15 +0530

    Matrix Multiplication

--- PATTERNS.cpp/ARRAY/Vector/Matrix_Multiplication.cpp
+++ PATTERNS.cpp/ARRAY/Vector/Matrix_Multiplication.cpp
@@ -6,5 +6,27 @@ int main(){
     cin>>n>>m;
     vector<vector<int>>a(n,vector<int>(m));
      vector<vector<int>>b(n,vector<int>(m));
-for(inti=0;i<n;i++)
+for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+        cin>>a[i][j];
+    }
+}
+for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+        cin>>b[i][j];
+}
+}
+ vector<vector<int>>c(n,vector<int>(m));
+for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+        c[i][j]=a[i][j]*b[i][j];
+    }
+}
+for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+        cout<<c[i][j]<<" ";
+}
+cout<<endl;
+}
+return 0;
 }
